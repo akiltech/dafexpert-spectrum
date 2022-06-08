ff7ece08b6a5bbc228b2326e846f251e0c03df65
Amara Bamba <amara.dianney@gmail.com>
2022-06-08 16:45:55 +0000

    :sparkles: feat(config): setting up commitizen, husky, eslint and prettier

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
-};
+import React from 'react';
+
+export default function Button() {
+  return <button type="button">Boop</button>;
+}
